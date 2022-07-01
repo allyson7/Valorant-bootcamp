707decd9a6082c2a955c6e661cf743529754c4f3
allyson7 <allysonlopes0@gmail.com>
2022-07-01 18:01:00 -0400

    :art: Configuring apollo and graphql

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,23 @@
+import { gql } from "@apollo/client";
+import { useEffect } from "react";
+import { client } from "./lib/apollo";
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+`;
+
 function App() {
+  useEffect(() => {
+    client.query({
+      query: GET_LESSONS_QUERY,
+    });
+  }, []);
+
   return (
     <h1 className="text-5xl font-bold text-red-valorant">
       Hello Valorant Agents
