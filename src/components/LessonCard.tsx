7956fcc8055843f5e6628646c833e2ef7d41dcc7
allyson7 <allysonlopes0@gmail.com>
2022-07-29 18:28:43 -0400

    :sparkles: Created sidebar and lessons info no portal

--- src/components/LessonCard.tsx
+++ src/components/LessonCard.tsx
@@ -1,15 +1,45 @@
-export function LessonCard() {
+import { CheckCircle, Lock } from "phosphor-react";
+import { isPast, format } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
+
+interface LessonCardProps {
+  title: string;
+  slug: string;
+  availableAt: Date;
+  type: "live" | "lesson";
+}
+
+export function LessonCard(props: LessonCardProps) {
+  const isLessonAvailable = isPast(props.availableAt);
+  const availableDateFormatted = format(
+    props.availableAt,
+    "EEEE ' • ' d' de 'MMMM' • 'k'h'mm ",
+    { locale: ptBR }
+  );
+
   return (
     <a href="#">
-      <span className="text-gray-300">Domingo • 20 de junho • 19h00</span>
+      <span className="text-gray-300">{availableDateFormatted}</span>
 
       <div className="rounded border border-gray-500 p-4 mt-2">
-        <header>
-          <span>Conteúdo Liberado</span>
-          <span>AO VIVO</span>
+        <header className="flex items-center justify-between">
+          {isLessonAvailable ? (
+            <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
+              <CheckCircle size={20} />
+              Conteúdo Liberado
+            </span>
+          ) : (
+            <span className="text-sm text-orange-500 font-medium flex items-center gap-2">
+              <Lock size={20} />
+              Em breve
+            </span>
+          )}
+          <span className="text-xs rounded px-2 py-[0.125rem] text-white border border-green-300 font-bold">
+            {props.type === "live" ? "AO VIVO" : "AULA PRÁTICA"}
+          </span>
         </header>
 
-        <strong>Abertura do evento Ignite labs</strong>
+        <strong className="text-gray-200 mt-5 block">{props.title}</strong>
       </div>
     </a>
   );
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,31 @@
+import { gql, useQuery } from "@apollo/client";
 import { LessonCard } from "./LessonCard";
 
+const GET_LESSONS_QUERY = gql`
+  query MyQuery {
+    lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
+      id
+      lessonType
+      availableAt
+      title
+      slug
+    }
+  }
+`;
+
+interface GetLessonsQueryResponse {
+  lessons: {
+    id: string;
+    title: string;
+    slug: string;
+    availableAt: string;
+    lessonType: "live" | "lesson";
+  }[];
+}
+
 export function Sidebar() {
+  const { data } = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY);
+
   return (
     <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
       <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
@@ -8,12 +33,17 @@ export function Sidebar() {
       </span>
 
       <div className="flex flex-col gap-8">
-        <LessonCard />
-        <LessonCard />
-        <LessonCard />
-        <LessonCard />
-        <LessonCard />
-        <LessonCard />
+        {data?.lessons.map((lesson) => {
+          return (
+            <LessonCard
+              key={lesson.id}
+              title={lesson.title}
+              slug={lesson.slug}
+              availableAt={new Date(lesson.availableAt)}
+              type={lesson.lessonType}
+            />
+          );
+        })}
       </div>
     </aside>
   );
