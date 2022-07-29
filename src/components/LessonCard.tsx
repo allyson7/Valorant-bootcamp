5d05a15b8ef3c23b33e909e305fe4eac50f2cc12
allyson7 <allysonlopes0@gmail.com>
2022-07-29 17:24:56 -0400

    :construction: Half way to go on lesson 2

---
+++ src/components/LessonCard.tsx
@@ -0,0 +1,16 @@
+export function LessonCard() {
+  return (
+    <a href="#">
+      <span className="text-gray-300">Domingo • 20 de junho • 19h00</span>
+
+      <div className="rounded border border-gray-500 p-4 mt-2">
+        <header>
+          <span>Conteúdo Liberado</span>
+          <span>AO VIVO</span>
+        </header>
+
+        <strong>Abertura do evento Ignite labs</strong>
+      </div>
+    </a>
+  );
+}
