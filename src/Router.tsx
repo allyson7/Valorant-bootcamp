16838e618fa9a90232c8e8a4d8040861ac8ef0ac
allyson7 <allysonlopes0@gmail.com>
2022-08-01 16:30:13 -0400

    :sparkles: Finished routes configuration and lesson 3

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,9 +1,12 @@
 import { Route, Routes } from "react-router-dom";
+import { Event } from "./pages/Event";
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" />
+      <Route path="/" element={<h1>Home</h1>} />
+      <Route path="/event" element={<Event />} />
+      <Route path="/event/lesson/:slug" element={<Event />} />
     </Routes>
   );
 }
--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -6,16 +6,63 @@ import {
   ImageSquare,
   Lightning,
 } from "phosphor-react";
+import { gql, useQuery } from "@apollo/client";
 
 import "@vime/core/themes/default.css";
 
-export function Video() {
+const GET_LESSON_BY_SLUG_QUERY = gql`
+  query getLessonSlug($slug: String) {
+    lesson(where: { slug: $slug }) {
+      title
+      videoId
+      description
+      teacher {
+        bio
+        avatarURL
+        name
+      }
+    }
+  }
+`;
+
+interface GetLessonBySlugResponse {
+  lesson: {
+    title: string;
+    videoId: string;
+    description: string;
+    teacher: {
+      bio: string;
+      avatarURL: string;
+      name: string;
+    };
+  };
+}
+
+interface VideoProps {
+  lessonSlug: string;
+}
+
+export function Video(props: VideoProps) {
+  const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
+    variables: {
+      slug: props.lessonSlug,
+    },
+  });
+
+  if (!data) {
+    return (
+      <div className="flex-1">
+        <p>Carregando...</p>
+      </div>
+    );
+  }
+
   return (
     <div className="flex-1">
       <div className="bg-black flex justify-center">
         <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
           <Player>
-            <Youtube videoId="mCh9Z656YQs" />
+            <Youtube videoId={data.lesson.videoId} />
             <DefaultUi />
           </Player>
         </div>
@@ -24,28 +71,23 @@ export function Video() {
       <div className="p-8 max-w-[1100px] mx-auto">
         <div className="flex items-start gap-16">
           <div className="flex-1 ">
-            <h1 className="text-2xl font-bold">
-              Aula 01 - Abertura do bootcamp de Valorant
-            </h1>
+            <h1 className="text-2xl font-bold">{data.lesson.title}</h1>
             <p className="mt-4 text-gray-200 leading-relaxed">
-              Vencer a rodada de pistola é super importante no VALORANT pois ela
-              define qual time vai ter mais créditos no segundo round. No
-              episódio de hoje, o jogador Arthur Geronimo vai dar algumas dicas
-              sobre como atacar na rodada de pistola!
+              {data.lesson.description}
             </p>
 
             <div className="flex items-center gap-4 mt-6">
               <img
                 className="h-16 w-16 rounded-full border-2 border-blue-500"
-                src="https://github.com/ArrthurGeronimo.png"
+                src={data.lesson.teacher.avatarURL}
                 alt="Foto de perfil do professor"
               />
               <div className="leading-relaxed">
                 <strong className="font-bold text-2xl block">
-                  Arthur Geronimo
+                  {data.lesson.teacher.name}
                 </strong>
                 <span className="text-gray-200 text-sm block">
-                  Desenvolvedor e instrutor profissional de FPS
+                  {data.lesson.teacher.bio}
                 </span>
               </div>
             </div>
